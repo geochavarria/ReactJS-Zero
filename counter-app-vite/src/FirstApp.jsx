4150c94e3db54ef793f1c00a234ad47a0f2b0292
EdwinDeskHome <echavarria@ugb.edu.sv>
2023-11-02 17:29:34 -0600

    Impresión de variables en el HTML

--- counter-app-vite/src/FirstApp.jsx
+++ counter-app-vite/src/FirstApp.jsx
@@ -1,15 +1,18 @@
 import { Fragment } from "react"
 
+const newMessage = "Geo!!!"
 
+
+const getResult = (a,b) => {
+    return a + b;
+}
 export const FirstApp = () => {
+
+    
     return(
-        // <Fragment>
-        //     <h1>Geo Chavarria</h1>
-        //     <p>Spy con subtitulo</p>
-        // </Fragment>
         <>
-            <h1>Geo Chavarria</h1>
-            <p>Spy con subtitulo</p>
+            <h1>{  getResult(1,9) }</h1>
+            <p>Soy con subtitulo</p>
         </>
     )
 }
