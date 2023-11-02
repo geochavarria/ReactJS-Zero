36f8e29c601af2a81b4b28febba7d4cfc567c128
EdwinDeskHome <echavarria@ugb.edu.sv>
2023-11-02 17:54:18 -0600

    Comunicación entre componentes - Props

--- counter-app-vite/src/FirstApp.jsx
+++ counter-app-vite/src/FirstApp.jsx
@@ -1,16 +1,9 @@
 import { Fragment } from "react"
-
-const newMessage = "Geo!!!"
-
-
-const getResult = (a,b) => {
-    return a + b;
-}
-export const FirstApp = () => {
+export const FirstApp = ({ title, subtitle}) => {
     return(
         <>
-            <h1>{  getResult(1,9) }</h1>
-            <p>Soy con subtitulo</p>
+            <h1>{ title }</h1>
+            <p>{ subtitle }</p>
         </>
     )
 }
--- counter-app-vite/src/main.jsx
+++ counter-app-vite/src/main.jsx
@@ -8,6 +8,6 @@ import "./styles.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         {/* <HelloWorldApp /> */}
-        <FirstApp />
+        <FirstApp title="Hola soy Geo" subtitle ="123" />
     </React.StrictMode>
 );
