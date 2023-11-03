33701ca7333cf84d65dcc35569418481d46f90fd
EdwinDeskHome <echavarria@ugb.edu.sv>
2023-11-02 18:22:59 -0600

    handleSubtract y handleReset

--- counter-app-vite/src/CounterApp.jsx
+++ counter-app-vite/src/CounterApp.jsx
@@ -6,10 +6,13 @@ export const CounterApp = ({value}) => {
 
     const [counter, setCounter ] = useState(value)
 
-    const  handledAdd = (event) => {
+    const  handleAdd = (event) => {
         //setCounter(counter + 1)
         setCounter((c)=> c + 1 )
     }
+
+    const handleSubstract = () => setCounter(counter -1);
+    const handleReset = () => setCounter(value);
 
 
     return (
@@ -17,7 +20,9 @@ export const CounterApp = ({value}) => {
             <h1>CounterApp</h1>
             <h2>{ counter }</h2>
 
-            <button onClick={ handledAdd }>+1</button>
+            <button onClick={ handleAdd }>+1</button>
+            <button onClick={ handleSubstract }>-1</button>
+            <button onClick={ handleReset }>Reset</button>
         </React.Fragment>
     )
 }
