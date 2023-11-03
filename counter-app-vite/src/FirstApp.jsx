3c2b9ae421fad23096c106e9f05423349b2bc38b
EdwinDeskHome <echavarria@ugb.edu.sv>
2023-11-02 18:05:24 -0600

    DefaultProps

--- counter-app-vite/src/FirstApp.jsx
+++ counter-app-vite/src/FirstApp.jsx
@@ -1,10 +1,14 @@
 import { Fragment } from "react"
 import PropTypes from "prop-types"
-export const FirstApp = ({ title, subtitle}) => {
+export const FirstApp = ({ 
+    title, 
+    subtitle,
+    name}) => {
     return(
         <>
             <h1>{ title }</h1>
             <p>{ subtitle }</p>
+            <p>{ name }</p>
         </>
     )
 }
@@ -12,5 +16,11 @@ export const FirstApp = ({ title, subtitle}) => {
 
 FirstApp.propTypes ={
     title : PropTypes.string.isRequired,
-    subtitle: PropTypes.string.isRequired
+    subtitle: PropTypes.number
+}
+
+FirstApp.defaultProps ={
+    title : "No Hay titulo",
+    subtitle: "No hay subtitulo",
+    name : "Geo Chavarria"
 }
--- counter-app-vite/src/main.jsx
+++ counter-app-vite/src/main.jsx
@@ -8,6 +8,6 @@ import "./styles.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         {/* <HelloWorldApp /> */}
-        <FirstApp title={"Soy Geo"} subtitle="This is a Subtitle"/>
+        <FirstApp />
     </React.StrictMode>
 );
