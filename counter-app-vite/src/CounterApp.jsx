97ecd774165a51afc8d488633a36bc00aba368c3
EdwinDeskHome <echavarria@ugb.edu.sv>
2023-11-02 18:20:07 -0600

    useState - Hook

--- counter-app-vite/src/CounterApp.jsx
+++ counter-app-vite/src/CounterApp.jsx
@@ -1,20 +1,21 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 
 
 export const CounterApp = ({value}) => {
 
+    const [counter, setCounter ] = useState(value)
 
     const  handledAdd = (event) => {
-        console.log("+1")
-        value = 1000
-        console.log(value)
+        //setCounter(counter + 1)
+        setCounter((c)=> c + 1 )
     }
+
 
     return (
         <React.Fragment>
             <h1>CounterApp</h1>
-            <h2>{ value }</h2>
+            <h2>{ counter }</h2>
 
             <button onClick={ handledAdd }>+1</button>
         </React.Fragment>
