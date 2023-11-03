4ea8dbc55159bab1ce973a06c9349327773135b5
EdwinDeskHome <echavarria@ugb.edu.sv>
2023-11-02 19:45:39 -0600

    Jest - Expect - toBe

--- README.md
+++
@@ -1,2 +0,0 @@
-# ReactJS-Zero
-React JS Begginer
--- counter-app-vite/tests/demo.test.jsx
+++ counter-app-vite/tests/demo.test.jsx
@@ -1,8 +1,16 @@
 
+describe("Pruebas <DemoComponent />", () => {
+    test('Esta prueba no debe fallar', () => {
+        ///1 Inicializacion
+        const message1 = "Hola mundo";
+    
+        ///2 Estimulo
+        const message2 = message1.trim()
+    
+        ///3 Observar el comportamiento
+        expect(message1).toBe(message2)
+    
+    })
 
-
-test('Esta prueba no debe fallar', () => {
-    if(1=== 0){
-        throw new Error("No puede dividir entre cero")
-    }
 })
+
