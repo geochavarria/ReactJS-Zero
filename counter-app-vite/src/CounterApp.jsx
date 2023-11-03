408634632a55a3bf66873e5fd794d769c0e04099
EdwinDeskHome <echavarria@ugb.edu.sv>
2023-11-02 18:15:01 -0600

    Evento click (Eventos en general)

--- counter-app-vite/src/CounterApp.jsx
+++ counter-app-vite/src/CounterApp.jsx
@@ -5,10 +5,18 @@ import PropTypes from "prop-types"
 export const CounterApp = ({value}) => {
 
 
+    const  handledAdd = (event) => {
+        console.log("+1")
+        value = 1000
+        console.log(value)
+    }
+
     return (
         <React.Fragment>
             <h1>CounterApp</h1>
             <h2>{ value }</h2>
+
+            <button onClick={ handledAdd }>+1</button>
         </React.Fragment>
     )
 }
